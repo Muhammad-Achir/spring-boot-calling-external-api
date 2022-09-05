420b08722558f00b29bac2c5740f8af9524c6b7e
armi-ops <muhachir.sangnila@gmail.com>
2022-09-05 12:19:28 +0700

    make cors-safe

--- src/main/java/com/lastdeveloper/springbootcallingexternalapi/SpringBootCallingExternalApiApplication.java
+++ src/main/java/com/lastdeveloper/springbootcallingexternalapi/SpringBootCallingExternalApiApplication.java
@@ -2,6 +2,9 @@ package com.lastdeveloper.springbootcallingexternalapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SpringBootCallingExternalApiApplication {
@@ -9,5 +12,15 @@ public class SpringBootCallingExternalApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootCallingExternalApiApplication.class, args);
 	}
-
+        
+        @Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("/photos-url").allowedOrigins("http://localhost:8080");
+				registry.addMapping("/*").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 }
--- src/main/java/com/lastdeveloper/springbootcallingexternalapi/controller/RequestAPIController.java
+++ src/main/java/com/lastdeveloper/springbootcallingexternalapi/controller/RequestAPIController.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -80,6 +81,7 @@ public class RequestAPIController {
         return ResponseEntity.ok(photoItems);
     }
     
+//    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/photos-url")
     public List<PhotoUrlResponse> getPhotosUrl() {
         List<PhotoUrlResponse> result = new ArrayList<>();
